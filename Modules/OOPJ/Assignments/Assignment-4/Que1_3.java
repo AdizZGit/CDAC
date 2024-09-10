e632da7dac0bf3ee418b9369f06d4d9188ae3f37
AdizZGit <145923553+AdizZGit@users.noreply.github.com>
2024-09-11 00:22:08 +0530

    --

--- Modules/OOPJ/Assignments/Assignment-4/Que1_3.java
+++ Modules/OOPJ/Assignments/Assignment-4/Que1_3.java
@@ -1,3 +1,13 @@
+/*. Loan Amortization Calculator
+Implement a system to calculate and display the monthly payments for a mortgage loan. The system should:
+1.	Accept the principal amount (loan amount), annual interest rate, and loan term (in years) from the user.
+2.	Calculate the monthly payment using the standard mortgage formula:
+o	Monthly Payment Calculation:
+	monthlyPayment = principal * (monthlyInterestRate * (1 + monthlyInterestRate)^(numberOfMonths)) / ((1 + monthlyInterestRate)^(numberOfMonths) - 1)
+	Where monthlyInterestRate = annualInterestRate / 12 / 100 and numberOfMonths = loanTerm * 12
+	Note: Here ^ means power and to find it you can use Math.pow( ) method
+3.	Display the monthly payment and the total amount paid over the life of the loan, in Indian Rupees (₹).
+*/
 package pack1;
 import java.util.Scanner;
 
