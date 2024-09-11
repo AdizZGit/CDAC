7ab9ff9e55be74ab997f67134fb420e5100bebfd
AdizZGit <145923553+AdizZGit@users.noreply.github.com>
2024-09-12 00:02:48 +0530

    --

---
+++ Modules/OOPJ/Daily_Work/Student.java
@@ -0,0 +1,45 @@
+package First;
+
+public class Student {
+		String name;
+		int id;
+		
+		{
+			name="adi";
+			id=12;
+		}
+		
+		private  void display() {
+			System.out.println("Name :"+this.name);
+			System.out.println("Id :"+this.id);
+			
+		}
+		public static void main(String[] args) {
+			Student s=new Student();
+			s.display();
+		}
+}
+
+//		void display() { //default
+//			System.out.println("Name :"+this.name);
+//			System.out.println("Id :"+this.id);
+//			
+//		}
+//		protected void display() {
+//			System.out.println("Name :"+this.name);
+//			System.out.println("Id :"+this.id);
+//			
+//		}
+//		public static void main(String[] args) { //package level private is accessed within the same class
+//			Student s=new Student();
+//			s.display();
+//		}
+		
+
+// public class Student extends Ass{
+//	 public static void main(String[] args) {
+//	Ass a=new Ass(); //protected can be accessed into subclass
+//	a.display();
+//}
+// }
+
---
+++ Modules/OOPJ/Daily_Work/Test.java
@@ -0,0 +1,34 @@
+package Second;
+
+import First.*;
+
+public class Test extends Student{
+	public static void main(String[] args) {
+		Test t=new Test();
+		t.display();//private is not accessed into subclass of other package
+	}
+	
+}	
+//	public static void main(String[] args) {
+//		Test t=new Test();
+//		t.display();//protected is accessed into subclass of other package
+	
+//	public static void main(String[] args) {
+//		Student s=new Student();
+//		s.display(); // protected does not allows to access the methods into other package class
+//	}
+	
+	
+//	public static void main(String[] args) { //public can be accessed anywhere
+//		Student s=new Student();
+//		s.display();
+//
+//	}
+		
+//	public static void main(String[] args) {
+//		Student s=new Student();
+//		s.display();//The method display() from the type Student is not visible
+////default cannot be accessed into other packages
+//	}
+	
+}
