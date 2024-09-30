f5d444917bbf14c78860a37c68a449fb49a7cd40
AdizZGit <145923553+AdizZGit@users.noreply.github.com>
2024-09-30 23:35:00 +0530

    --

--- Modules/ADS/Daily_Work/LLinsertion.java
+++ Modules/ADS/Daily_Work/LLinsertion.java
@@ -29,6 +29,32 @@ public class LLinsertion{
 		newnode.next=prev.next;
 		prev.next=newnode;
 	}
+	void del(int d){
+		Node temp=head, prev=null;
+		if(temp!=null && temp.data==d){
+			head=temp.next; return;
+		}
+		while(temp!=null && temp.data!=d){
+			prev=temp;
+			temp=temp.next;
+		}
+		if(temp==null) return;
+		prev.next=temp.next;
+	}
+	void DelByPos(int p){
+		if(head==null) return;
+		Node temp=head;
+		if(p==0){
+			head=temp.next;
+		}
+		for(int i=0;temp!=null && i<p-1;i++){
+			temp=temp.next;
+		}
+		if(temp==null||temp.next==null) return;
+			Node next=temp.next.next;
+			temp.next=next;
+		
+	}
 	void dis(){
 		if(head==null){
 			System.out.println("Empty");
@@ -60,6 +86,14 @@ public class LLinsertion{
 		l.insAt(head,101);
 		l.insAt(head.next,201);
 		
+		l.dis();
+		System.out.print("\nDelete -----------------\n");
+		l.del(11);
+		//l.del(101);
+		//l.del(55);
+		l.dis();
+		System.out.print("\nDelete by pos-----------------\n");
+		l.DelByPos(0);
 		l.dis();
 
 	}
